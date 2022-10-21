e5fe9150d4373eb772e3708e87feef40155597e4
yinqin99 <95422153+suwakanda@users.noreply.github.com>
2022-10-21 13:44:04 +0800

    -

--- shop_db.sql
+++ shop_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 17, 2022 at 07:47 AM
+-- Generation Time: Oct 21, 2022 at 07:42 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -54,15 +54,9 @@ CREATE TABLE `cart` (
   `price` int(10) NOT NULL,
   `quantity` int(10) NOT NULL,
   `image` varchar(100) NOT NULL,
-  `discount` int(10) NOT NULL
+  `discount` int(10) NOT NULL,
+  `stock` int(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `cart`
---
-
-INSERT INTO `cart` (`id`, `user_id`, `pid`, `name`, `price`, `quantity`, `image`, `discount`) VALUES
-(9, 1, 2, 'Apple Macbook Pro 2021', 8888, 1, 'WhatsApp 1.jpg', 10);
 
 -- --------------------------------------------------------
 
@@ -128,19 +122,21 @@ CREATE TABLE `products` (
   `image_02` varchar(100) NOT NULL,
   `image_03` varchar(100) NOT NULL,
   `category` varchar(100) NOT NULL,
-  `discount` int(10) DEFAULT NULL
+  `discount` int(10) DEFAULT NULL,
+  `stock` int(100) NOT NULL,
+  `isActive` tinyint(10) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `products`
 --
 
-INSERT INTO `products` (`id`, `name`, `details`, `price`, `image_01`, `image_02`, `image_03`, `category`, `discount`) VALUES
-(1, 'Samsung S22ultra', 'The Samsung Galaxy S22 Ultra delivers a built-in S Pen, brighter display and lots of camera upgrades in a sleek design\r\nThis Galaxy S22 Ultra review proves that Samsung knows what power users want. This new beast adds the Galaxy Notes best feature while also offering a ton of other upgrades.\r\nYup, there is an S Pen built-in, but there is a lot more going on with this flagship as Samsung tries to top our best phone list.\r\n', 5000, 's221.JPG', 's222.JPG', 's223.JPG', 'smartphone', 50),
-(2, 'Apple Macbook Pro 2021', 'Apple in October 2021 overhauled the high-end MacBook Pro, introducing an entirely new design, new chips, new capabilities, and more. As Apple says, the revamped MacBook Pro models offer up extraordinary performance and it is the world best notebook display.\r\n\r\nThe 2021 MacBook Pro models come in 14.2-inch and 16.2-inch size options and they are equipped with mini-LED displays, more ports, up to 64GB memory, and more powerful Apple silicon chips, the M1 Pro and M1 Max. ', 9000, 'WhatsApp 1.jpg', 'WhatsApp Image 2.jpg', 'WhatsApp Image 3.jpg', 'laptop', 10),
-(8, 'Apple Watch Series 8', 'Apple Watch Series 8 uses the temperature sensor to provide women with retrospective ovulation estimates, which can be helpful for family planning purposes. The data also provides improved period predictions, and in watchOS 9, women can receive notifications if a logged cycle history shows a possible deviation like irregular, infrequent, or prolonged periods.', 4000, 'applewatch1.jpg', 'applewatch2.jpg', 'applewatch3.jpg', 'watch', 10),
-(9, 'Apple Magic Mouse 2', 'Magic Mouse is wireless and rechargeable, with an optimized foot design that lets it glide smoothly across your desk. The Multi-Touch surface allows you to perform simple gestures such as swiping between web pages and scrolling through documents.\r\n\r\nThe rechargeable battery will power your Magic Mouse for about a month or more between charges. It’s ready to go right out of the box and pairs automatically with your Mac.', 300, 'mouse1.jpg', 'mouse2.jpg', '', 'mouse', 10),
-(10, 'Sony ZV-E10', 'Designed for creative vloggers who aspire to an artistic look, the ZV-E10 takes care of the nuts and bolts of video production. Create a stunning vlog by taking advantage of interchangeable lenses, the large APS-C sensor, the built-in Directional 3-Capsule Mic and special features designed for vlogging – for a professional-looking vlog without the hassle.\r\n\r\nWith the new version software, Real-time Eye AF for animal is available when shooting movie. ', 3200, 'sonyzv1.jpg', 'songzy2.jpg', 'songzv3.jpg', 'camera', 10);
+INSERT INTO `products` (`id`, `name`, `details`, `price`, `image_01`, `image_02`, `image_03`, `category`, `discount`, `stock`, `isActive`) VALUES
+(1, 'Samsung S22ultra', 'The Samsung Galaxy S22 Ultra delivers a built-in S Pen, brighter display and lots of camera upgrades in a sleek design\r\nThis Galaxy S22 Ultra review proves that Samsung knows what power users want. This new beast adds the Galaxy Notes best feature while also offering a ton of other upgrades.\r\nYup, there is an S Pen built-in, but there is a lot more going on with this flagship as Samsung tries to top our best phone list.\r\n', 5000, 's221.JPG', 's222.JPG', 's223.JPG', 'smartphone', 10, 3, 0),
+(2, 'Apple Macbook Pro 2021', 'Apple in October 2021 overhauled the high-end MacBook Pro, introducing an entirely new design, new chips, new capabilities, and more. As Apple says, the revamped MacBook Pro models offer up extraordinary performance and it is the world best notebook display.\r\n\r\nThe 2021 MacBook Pro models come in 14.2-inch and 16.2-inch size options and they are equipped with mini-LED displays, more ports, up to 64GB memory, and more powerful Apple silicon chips, the M1 Pro and M1 Max. ', 9000, 'WhatsApp 1.jpg', 'WhatsApp Image 2.jpg', 'WhatsApp Image 3.jpg', 'laptop', 20, 10, 0),
+(3, 'Apple Watch Series 8', 'Apple Watch Series 8 uses the temperature sensor to provide women with retrospective ovulation estimates, which can be helpful for family planning purposes. The data also provides improved period predictions, and in watchOS 9, women can receive notifications if a logged cycle history shows a possible deviation like irregular, infrequent, or prolonged periods.', 4000, 'applewatch1.jpg', 'applewatch2.jpg', 'applewatch3.jpg', 'watch', 10, 10, 0),
+(4, 'Apple Magic Mouse 2', 'Magic Mouse is wireless and rechargeable, with an optimized foot design that lets it glide smoothly across your desk. The Multi-Touch surface allows you to perform simple gestures such as swiping between web pages and scrolling through documents.\r\n\r\nThe rechargeable battery will power your Magic Mouse for about a month or more between charges. It’s ready to go right out of the box and pairs automatically with your Mac.', 300, 'mouse1.jpg', 'mouse2.jpg', '', 'mouse', 10, 7, 0),
+(5, 'Sony ZV-E10', 'Designed for creative vloggers who aspire to an artistic look, the ZV-E10 takes care of the nuts and bolts of video production. Create a stunning vlog by taking advantage of interchangeable lenses, the large APS-C sensor, the built-in Directional 3-Capsule Mic and special features designed for vlogging – for a professional-looking vlog without the hassle.\r\n\r\nWith the new version software, Real-time Eye AF for animal is available when shooting movie. ', 3200, 'sonyzv1.jpg', 'songzy2.jpg', 'songzv3.jpg', 'camera', 10, 8, 0);
 
 -- --------------------------------------------------------
 
@@ -176,15 +172,16 @@ CREATE TABLE `wishlist` (
   `name` varchar(100) NOT NULL,
   `price` int(100) NOT NULL,
   `image` varchar(100) NOT NULL,
-  `discount` int(100) NOT NULL
+  `discount` int(100) NOT NULL,
+  `stock` int(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `wishlist`
 --
 
-INSERT INTO `wishlist` (`id`, `user_id`, `pid`, `name`, `price`, `image`, `discount`) VALUES
-(7, 1, 10, 'Sony ZV-E10', 3200, 'sonyzv1.jpg', 10);
+INSERT INTO `wishlist` (`id`, `user_id`, `pid`, `name`, `price`, `image`, `discount`, `stock`) VALUES
+(22, 1, 1, 'Samsung S22ultra', 5000, 's221.JPG', 10, 3);
 
 --
 -- Indexes for dumped tables
@@ -240,19 +237,19 @@ ALTER TABLE `wishlist`
 -- AUTO_INCREMENT for table `admins`
 --
 ALTER TABLE `admins`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT for table `messages`
 --
 ALTER TABLE `messages`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `orders`
@@ -270,13 +267,13 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `wishlist`
 --
 ALTER TABLE `wishlist`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
