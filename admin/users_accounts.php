1edbfe47ba3f2f2b4f148610915b11a9effb860f
yinqin99 <95422153+suwakanda@users.noreply.github.com>
2022-10-22 19:00:07 +0800

    -

--- admin/users_accounts.php
+++ admin/users_accounts.php
@@ -38,53 +38,90 @@ if(isset($_GET['delete'])){
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
 
    <link rel="stylesheet" href="../css/admin_style.css">
+   <link rel="stylesheet" href="assets/dataTables.bootstrap4.min.css">
+   <link rel="stylesheet" href="assets/bootstrap.min.css">
 
 </head>
 <body>
 
 <?php include '../components/admin_header.php'; ?>
 
-<section class="accounts">
 
-   <h1 class="heading">user accounts</h1>
+<section class="table-product" >
+<h1 class="heading">user accounts</h1>
+<div class="card " style="width: 100%;">
 
-   <div class="box-container">
+        <div class="card-header">
+        <div class="card-body pr-2 pl-2">
 
-   <?php
-      $select_accounts = $conn->prepare("SELECT * FROM `users`");
-      $select_accounts->execute();
-      if($select_accounts->rowCount() > 0){
-         while($fetch_accounts = $select_accounts->fetch(PDO::FETCH_ASSOC)){   
-   ?>
-   <div class="box">
-      <p> user id : <span><?= $fetch_accounts['id']; ?></span> </p>
-      <p> username : <span><?= $fetch_accounts['name']; ?></span> </p>
-      <p> email : <span><?= $fetch_accounts['email']; ?></span> </p>
-      <a href="users_accounts.php?delete=<?= $fetch_accounts['id']; ?>" onclick="return confirm('delete this account? the user related information will also be delete!')" class="delete-btn">delete</a>
-   </div>
-   <?php
-         }
-      }else{
-         echo '<p class="empty">no accounts available!</p>';
-      }
-   ?>
+          <table id="example" class="table table-striped table-bordered" style="width:100%">
+                  <thead>
+                    <tr>
+                      <th class="text-center">Product id</th>
+                      <th  class="text-center">Name</th>
+                      <th  class="text-center">Email</th>
+                      <th  class="text-center">Phone</th> 
+                      <th  width='25%' class="text-center">Action</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                  <?php
+                     $select_accounts = $conn->prepare("SELECT * FROM `users`");
+                     $select_accounts->execute();
+                     if($select_accounts->rowCount() > 0){
+                        while($fetch_accounts = $select_accounts->fetch(PDO::FETCH_ASSOC)){   
+                  ?>
+                  
+                  <tr class="text-center">
+                  
+                    <td><?php echo $fetch_accounts['id'];?></td>
+                    <td><a href="update_product.php?update=<?php echo $fetch_accounts['id'];?>"><?php echo $fetch_accounts['name'];?></a></td>
+                    
+                    <td><?php echo $fetch_accounts['email']; ?></td>
+                    <td><?php echo $fetch_accounts['phone']; ?></td>
+                   
+                    <td>
+                     <a onclick="return confirm('Are you sure To Delete ? The user related information will also be delete!')" class="btn btn-danger btn-sm " href="users_accounts.php?delete=<?= $fetch_accounts['id']; ?>">Remove</a>     
+                    </td>
+                     
+                  
+                  <?php }?>
+                  <?php }else{
+                     echo '<p class="empty">no accounts available!</p>';
+                  }?>
+                  
+                  
+                  </tr>
+                  </tbody>
 
-   </div>
-
+                  
+          </table>
+        </div>
+        </div>
+</div>
 </section>
 
 
 
 
-
-
-
-
-
-
-
-
 <script src="../js/admin_script.js"></script>
+ <!-- Jquery script -->
+ <script src="assets/jquery.min.js"></script>
+  <script src="assets/bootstrap.min.js"></script>
+  <script src="assets/jquery.dataTables.min.js"></script>
+  <script src="assets/dataTables.bootstrap4.min.js"></script>
+  <script>
+      $(document).ready(function () {
+          $("#flash-msg").delay(7000).fadeOut("slow");
+      });
+
+      $(document).ready(function() {
+          $('#example').DataTable();
+          
+      } );
+      
+      
+  </script>
    
 </body>
 </html>
