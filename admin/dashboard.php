8b37ce978b24cddb9fb45b4295022c8b31e1dd7b
yinqin99 <95422153+suwakanda@users.noreply.github.com>
2022-11-07 00:04:51 +0800

    -

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -10,6 +10,15 @@ if(!isset($admin_id)){
    header('location:admin_login.php');
 }
 
+
+date_default_timezone_set('Asia/Kuala_Lumpur');
+
+$today = date('Y-m-d');
+$year = date('Y');
+if(isset($_GET['year'])){
+  $year = $_GET['year'];
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -27,6 +36,7 @@ if(!isset($admin_id)){
    <script src="https://cdn.jsdelivr.net/npm/chart.js@3.9.1/dist/chart.min.js"></script>
    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
    
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
    <link rel="stylesheet" href="../css/admin_style.css">
 
@@ -144,11 +154,7 @@ if(!isset($admin_id)){
 
 <!-- Chart Data -->
 <?php
-$today = date('Y-m-d');
-$year = date('Y');
-if(isset($_GET['year'])){
-  $year = $_GET['year'];
-}
+
 
 
   $months = array();
